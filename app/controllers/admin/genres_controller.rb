34d96275fbdb2cf9b74e1c1bed4516764418bb6e
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-15 07:33:29 +0000

    [Add]genreのsave,update機能とレイアウト

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -16,10 +16,13 @@ class Admin::GenresController < ApplicationController
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
-  
+
   private
-  
+
   def genre_params
     params.require(:genre).permit(:name)
   end
