f80d42b94839a953570149c2bc44fe587a160e05
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-15 06:18:21 +0000

    [Add]管理者用itemのアクション記述

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,24 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new
+    if @item.save
+      redirect_to request.referer
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 end
