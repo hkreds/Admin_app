4d883f50dfdb18f9716aca62bab77fa5bc5255f8
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-17 00:22:58 +0000

    [Add]orderモデルadmin用のorderコントローラー・ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :orders, only: [:index, :show]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
